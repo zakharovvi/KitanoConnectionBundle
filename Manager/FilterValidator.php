7d8fd8e11aad2aa145836165e5aebb6c53095433
Vitaliy Zakharov <v.zakharov@xsolla.com>
2015-06-28 03:03:19 +0500

    filter validator fix 2

--- Manager/FilterValidator.php
+++ Manager/FilterValidator.php
@@ -3,16 +3,9 @@
 namespace Kitano\ConnectionBundle\Manager;
 
 use Kitano\ConnectionBundle\Exception\InvalidFilterException;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Constraints\Type;
-use Symfony\Component\Validator\Constraints\Collection;
-use Symfony\Component\Validator\Validator\LegacyValidator;
 
 class FilterValidator
 {
-    protected $validator;
-
     /**
      * Validate and normalize input filters for connections retrieval
      *
@@ -22,15 +15,5 @@ class FilterValidator
     public function validateFilters(array &$filters)
     {
         return true;
-    }
-
-    public function setValidator(LegacyValidator $validator)
-    {
-        $this->validator = $validator;
-    }
-
-    public function getValidator()
-    {
-        return $this->validator;
     }
 }
